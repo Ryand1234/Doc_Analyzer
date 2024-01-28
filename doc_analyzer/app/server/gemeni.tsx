b99d4774f553c79ff29815f94177b55770c4c1e4
Riyan Dhiman <riyandhiman14@gmail.com>
2024-01-28 14:28:30 +0530

    fix

--- doc_analyzer/app/server/gemeni.tsx
+++ doc_analyzer/app/server/gemeni.tsx
@@ -1,8 +1,6 @@
-'use server';
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 // Access your API key as an environment variable (see "Set up your API key" above)
-console.log(process.env.GEMINI_API_KEY, process.env)
 const genAI = new GoogleGenerativeAI(process.env.NEXT_GEMINI_API_KEY);
 
 const MODEL_NAME = 'gemini-pro'
