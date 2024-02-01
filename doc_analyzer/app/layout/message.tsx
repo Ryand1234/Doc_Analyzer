3bda2b740e8d6f51b3fbd326ff72cba7cca938bf
Riyan Dhiman <riyandhiman14@gmail.com>
2024-02-01 09:28:35 +0530

    refactor

--- doc_analyzer/app/layout/message.tsx
+++ doc_analyzer/app/layout/message.tsx
@@ -1,32 +1,8 @@
 "use client";
-import { Breadcrumb, Layout, theme } from 'antd';
-import React, { useState, useEffect } from 'react';
-const { Header, Content, Footer, Sider } = Layout;
+import React from 'react';
 import { Info } from '../interface/chat-interface';
-import Image from 'next/image'
-import userImg from '../../public/user.png'
 import './message.css'
-import {
-    UserOutlined,
-} from '@ant-design/icons';
-import bot from '../../public/bot.png'
-// function loadScript(src: any) {
-//     return new Promise((resolve, reject) => {
-//       const scriptElement = document.createElement('script');
-//       scriptElement.type = 'text/javascript';
-//       scriptElement.src = src;
-//       scriptElement.onload = resolve;
-//       scriptElement.onerror = reject;
-//       document.head.appendChild(scriptElement);
-//     });
-//   }
-
-//   async function loadExternalScripts(scriptSrcs: any) {
-//     const scriptPromises = scriptSrcs.map(loadScript);
-//     await Promise.all(scriptPromises);
-//   }
 export default function Message({ user, hide, message }: Info) {
-    const [hidden, setHidden] = useState(true);
     const isHTML = /<[a-z][\s\S]*>/i.test(message);
     
     return (
@@ -43,8 +19,6 @@ export default function Message({ user, hide, message }: Info) {
             }}
             className={`message`}
             id={`${user.toLowerCase()}`}
-            onMouseEnter={() => { setHidden(false) }}
-            onMouseLeave={() => { setHidden(true) }}
         >
             {isHTML ? <span
                 id={`${user.toLowerCase()}-response`}
